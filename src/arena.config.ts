547b17441450824245a7c6df57a9585e174269c0
Vincent Ho <vinmaster99@gmail.com>
2023-01-27 12:07:05 -0800

    init commit

---
+++ src/arena.config.ts
@@ -0,0 +1,27 @@
+import Arena from '@colyseus/arena';
+import { monitor } from '@colyseus/monitor';
+import express from 'express';
+
+import { MyRoom } from './rooms/MyRoom';
+
+export default Arena({
+  getId: () => 'mark-my-words',
+
+  initializeGameServer: gameServer => {
+    gameServer.define('my_room', MyRoom);
+  },
+
+  initializeExpress: app => {
+    app.use(express.static(`${process.cwd()}/public`));
+    app.get('/', (req, res) => {
+      res.send("It's time to kick ass and chew bubblegum!");
+    });
+    app.use('/colyseus', monitor());
+  },
+
+  beforeListen: () => {
+    /**
+     * Before before gameServer.listen() is called.
+     */
+  },
+});
